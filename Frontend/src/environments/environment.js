de9a40100663bcd27b7d330acbacc0076d6aa7d1
SupunThilanka <e19349@eng.pdn.ac.lk>
2024-06-16 22:31:23 +0530

    Update environment.js

--- Frontend/src/environments/environment.js
+++ Frontend/src/environments/environment.js
@@ -1,5 +1,5 @@
 export const environment = {
     production:true,
-    baseUrl:'mysql://uvpo4u4ek0vuqojk:mmDzXoqje2y2unghWnq0@b6wavisrxih3nlmdwdo9-mysql.services.clever-cloud.com:3306/b6wavisrxih3nlmdwdo9'
+    baseUrl:'https://online-retail-system-esoo0hg7v-supunthilankas-projects.vercel.app/api/v1'
 
 };
