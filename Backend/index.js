e26d0c889a8437c68305114785a4963548b34751
SupunThilanka <e19349@eng.pdn.ac.lk>
2024-06-16 23:20:29 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use('/api/v1', (req, res, next) => {
     if (req.path === '/') {
-        res.send('Server is running');
+        res.send('Server is running on ${port}');
         console.log("Server is running on ${port}")
     } else {
         next();
