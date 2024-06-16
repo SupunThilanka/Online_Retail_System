8ff86bd9faf6ba6e307e1cc50fd77a88d9bb97a1
SupunThilanka <e19349@eng.pdn.ac.lk>
2024-06-16 21:41:26 +0530

    update databse.js

--- Backend/database.js
+++ Backend/database.js
@@ -1,20 +1,46 @@
-const mysql = require('mysql2');
 
 // const connection = mysql.createPool({
+    //     connectionLimit: 10,
+    //     host:"localhost",
+    //     user : "root",
+    //     password: "",
+    //     database: "retail_system",
+    //     multipleStatements: true // Enable multi-queries
+    // });
+    /*const connection = mysql.createPool({
+        connectionLimit: 10,
+        host:"bn67knmccgxrvirziqjl-mysql.services.clever-cloud.com",
+        user : "uxgx469bkivxrdm5",
+        password: "7t1sxm4EAbVO4ivcBOIR",
+        database: "bn67knmccgxrvirziqjl"
+        });*/
+
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST, 
+//     user: process.env.DB_USERNAME, 
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DBNAME,
+//     waitForConnections: true,
 //     connectionLimit: 10,
-//     host:"localhost",
-//     user : "root",
-//     password: "",
-//     database: "retail_system",
-//     multipleStatements: true // Enable multi-queries
+//     queueLimit: 0
 // });
-/*const connection = mysql.createPool({
-    connectionLimit: 10,
-    host:"bn67knmccgxrvirziqjl-mysql.services.clever-cloud.com",
-    user : "uxgx469bkivxrdm5",
-    password: "7t1sxm4EAbVO4ivcBOIR",
-    database: "bn67knmccgxrvirziqjl"
-});*/
+
+// // Attempt to connect to the database
+// pool.getConnection((err, connection) => {
+//     if (err) {
+//         console.error('Error connecting to database:', err);
+//         return;
+//     }
+//     console.log('Connected to the database!');
+//     connection.release(); // Release the connection back to the pool
+// });
+
+// module.exports = pool;
+
+const mysql = require('mysql2');
+
 const pool = mysql.createPool({
     host: process.env.DB_HOST, 
     user: process.env.DB_USERNAME, 
@@ -26,12 +52,13 @@ const pool = mysql.createPool({
 });
 
 // Attempt to connect to the database
-connection.getConnection((err) => {
+pool.getConnection((err, connection) => {
     if (err) {
         console.error('Error connecting to database:', err);
         return;
     }
     console.log('Connected to the database!');
+    connection.release(); // Release the connection back to the pool
 });
 
-module.exports = connection;
+module.exports = pool;
