e32401fb8fd904146b2f979c841c5e5fcd3a0a6a
SupunThilanka <e19349@eng.pdn.ac.lk>
2024-06-16 22:32:27 +0530

    Update environment.js

--- Frontend/src/environments/environment.js
+++ Frontend/src/environments/environment.js
@@ -1,5 +1,6 @@
 export const environment = {
     production:true,
+    // baseUrl:'http://localhost:8081/api/v1'
     baseUrl:'https://online-retail-system-esoo0hg7v-supunthilankas-projects.vercel.app/api/v1'
 
 };
