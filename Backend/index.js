62087da8676bdfd3c6c86f128316dc424b727afa
SupunThilanka <e19349@eng.pdn.ac.lk>
2024-06-17 09:01:15 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -9,10 +9,7 @@ const port = process.env.PORT || 8081;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
-// Middleware to handle /favicon.ico request
-app.get('/favicon.ico', (req, res) => res.status(204));
 
 // Root route handler for /api/v1
 app.get('/api/v1', (req, res) => {
@@ -22,10 +19,6 @@ app.get('/api/v1', (req, res) => {
 // Use the SystemRoute for other /api/v1 routes
 app.use('/api/v1', SystemRoute);
 
-// Catch-all route for undefined routes
-app.use((req, res) => {
-    res.status(404).send('Not Found');
-});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
