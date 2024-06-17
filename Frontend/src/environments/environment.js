7ec3958c73dc12a1d2a5d2981a7ad9e21302f12a
SupunThilanka <e19349@eng.pdn.ac.lk>
2024-06-17 09:33:40 +0530

    front end

--- Frontend/src/environments/environment.js
+++ Frontend/src/environments/environment.js
@@ -1,5 +1,5 @@
 export const environment = {
-    // production:true,
+    production:true,
     // // baseUrl:'http://localhost:8081/api/v1'
     baseUrl:'https://online-retail-system.vercel.app/api/v1'
     
